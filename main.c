1a24b86c64af670837d5af626fa2c740a76d78e3
BenicioPoli <beniciosanchezmandato@gmail.com>
2026-05-06 19:01:14 -0300

    agregado j y corregido nombres

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main()
     int flag = 1;
     while(flag){
         unsigned int instruccion = estado.program_memory[estado.pc];
-        switch(intruccion & 0b1111){
+        switch(instruccion & 0b1111){
         case 0:{ //add
             unsigned int guardar = (instruccion >> 4)& 0b1111;
             unsigned int v1 = (instruccion >> 8) & 0b1111;
@@ -26,32 +26,35 @@ int main()
         case 2:{ //lw
             unsigned int guardar = (instruccion >> 4) & 0b1111;
             unsigned int quelee = (instruccion >> 8) & 0b1111;
-            unsigned int desplazamiento = (intruccion >> 12) & 0b11111111;
+            unsigned int desplazamiento = (instruccion >> 12) & 0b11111111;
             estado.regs[guardar] = estado.data_memory[estado.regs[quelee] + desplazamiento];
             break; //el registro es para sacar bits porque sino tengo 8 nomas
         }
         case 3: { // sw
             unsigned int guardar = (instruccion >> 4) & 0b1111;
             unsigned int quelee = (instruccion >> 8) & 0b1111;
-            unsigned int desplazamiento = (intruccion >> 12) & 0b11111111;
+            unsigned int desplazamiento = (instruccion >> 12) & 0b11111111;
             estado.data_memory[estado.regs[guardar] + desplazamiento] = estado.regs[quelee];
-            break
+            break;
         }
         case 4: { //beq
             unsigned int v1 = (instruccion >> 4) & 0b1111;
             unsigned int v2 = (instruccion >> 8) & 0b1111;
             unsigned int salto = (instruccion >> 12) & 0b11111111;
-            if(estado.regs[v1] == estado.regs[v2]){
+            if(estado.regs[v1] == estado.regs[v2])
                 estado.pc += ((salto << 24) >> 24);
-            }
-        case 5: { // j
-
+            break;
         }
-
+        case 14: { // j
+            unsigned int salto = (instruccion >> 4) & 0b1111111111111111;
+            estado.pc += ((salto << 24) >> 24);
+            break;
         }
-        default:
-
+        default: {
+            printf("Error no se encuentra la instruccion");
+            break;
         }
+    }
     }
 
     return 0;
